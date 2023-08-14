8c428701feb43e9da589908fec74ff71256c8e75
masa-massara <masaya.yoshikawa.31@icloud.com>
2023-08-14 22:47:34 +0900

    var変数について

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 /* const、let等の変数宣言 */
+
+var val1 = "val変数";
+console.log(val1);
+
+// var変数の上書き
+val1 = "var変数を上書き";
+console.log(val1);
+
+//var変数の再宣言
+var val1 = "val変数を再宣言";
+console.log(val1);
