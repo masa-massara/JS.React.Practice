b0be869eee0d32d7f981300bb12e7a45d4e61222
masa-massara <masaya.yoshikawa.31@icloud.com>
2023-08-14 22:55:07 +0900

    let変数

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 /* const、let等の変数宣言 */
 
-var val1 = "val変数";
-console.log(val1);
+// var val1 = "val変数";
+// console.log(val1);
 
-// var変数の上書き
-val1 = "var変数を上書き";
-console.log(val1);
+// // var変数の上書き
+// val1 = "var変数を上書き";
+// console.log(val1);
 
-//var変数の再宣言
-var val1 = "val変数を再宣言";
-console.log(val1);
+// //var変数の再宣言
+// var val1 = "val変数を再宣言";
+// console.log(val1);
+
+
+let val2 = "let変数";
+console.log(val2);
+
+val2 = "let変数の上書き"
+console.log(val2);
+
+// letは再宣言不可！
