2cd8b5d26f641e7b3497215a1faf077ccd37899e
masa-massara <masaya.yoshikawa.31@icloud.com>
2023-08-15 19:47:41 +0900

    テンプレート文字列について

--- src/index.js
+++ src/index.js
@@ -41,11 +41,24 @@
 
 
 
-const val5 = ["dog","cat"];
+// const val5 = ["dog","cat"];
 
-val5[0]= "bird"
-val5.push("monkey");
-console.log(val5);
+// val5[0]= "bird"
+// val5.push("monkey");
+// console.log(val5);
 
-// //constで定義した配列はプロパティで上書き可能
+// // //constで定義した配列はプロパティで上書き可能
 
+//テンプレート文字列
+const name = "聖弥";
+const age = 19;
+
+//私の名前は聖弥です。年齢は１９歳です。
+
+//従来の方法
+const message1 = "私の名前は" + name + "です。年齢は" + age + "です。"
+console.log(message1);
+
+//テンプレート文字列を用いた方法
+const message2 = `私の名前は${name}です。年齢は${age}です。`
+console.log(message2);
